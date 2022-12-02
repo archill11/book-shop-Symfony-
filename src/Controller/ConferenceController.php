ee9426ab61fee145f1d99176b95bb41c3270c45b
Artur Shpachkov <artur2000sis@gmail.com>
2022-12-02 12:14:07 +0300

    init

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController {
+
+  #[Route('/conference', name: 'app_conference')]
+  public function index(): Response {
+    return $this->json(['test' => 'hello']);
+  }
+}
